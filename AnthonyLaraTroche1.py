aaa9ea24ce3c45c6b0f6171bf74c51789f34a298
Alara7771 <ujustgotmos@gmail.com>
2026-04-20 02:19:31 -0500

    added finish function and csv information

--- AnthonyLaraTroche1.py
+++ AnthonyLaraTroche1.py
@@ -1,5 +1,6 @@
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QMainWindow, QMessageBox
 from gui import *
+import csv
 class Vote(QMainWindow, Ui_Voting):
 
     def __init__(self):
@@ -18,6 +19,7 @@ class Vote(QMainWindow, Ui_Voting):
         self.candidate_2_vote = 0
 
         self.send_vote.clicked.connect(lambda : self.check())
+        self.finish_vote.clicked.connect(lambda : self.finish())
 
     def check(self):
         try:
@@ -58,9 +60,39 @@ class Vote(QMainWindow, Ui_Voting):
         except TypeError:
             self.main_label.setText("Name cannot contain numbers")
 
-        self.voter_list.append([self.first_name.text(), self.Last_name.text(), self.age_entry.text()])
+        self.voter_list.append([self.first_name.text(), self.Last_name.text(), self.age_entry.text(), self.vote_option])
+
+        self.first_name.clear()
+        self.Last_name.clear()
+        self.age_entry.clear()
+
+        self.rad_1.setAutoExclusive(False)
+        self.rad_1.setChecked(False)
+        self.rad_2.setChecked(False)
+        self.rad_1.setAutoExclusive(True)
+    def finish(self):
+        if len(self.voter_list) == 0:
+            return self.main_label.setText("There are no votes")
 
 
+        with open("results.csv", "w", newline="" ) as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["first_name", "last_name", "age", "Candidate"])
+            writer.writerows(self.voter_list)
+
+        self.close()
+        if self.candidate_1_vote > self.candidate_2_vote:
+            winner = f"Winner: {self.rad_1.text()}"
+        elif self.candidate_2_vote > self.candidate_1_vote:
+            winner = f"Winner: {self.rad_2.text()}"
+        else:
+            winner = "It's tied"
+
+        box = QMessageBox()
+        box.setWindowTitle("Voting Results")
+        box.setText("---Results---")
+        box.setText(f"{self.rad_1.text()} | {self.rad_2.text()}")
+        box.setText(winner)
 
 
 
